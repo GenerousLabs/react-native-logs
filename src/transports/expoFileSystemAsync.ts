be3e4d0929badbadd1c1e8f752f2d10cabd606a1
Callum Macdonald <git@callum-macdonald.com>
2020-12-04 11:43:38 +0100

    Handle first file read gracefully.

--- src/transports/expoFileSystemAsync.ts
+++ src/transports/expoFileSystemAsync.ts
@@ -6,17 +6,33 @@ const FileSystem = require('expo-file-system');
 
 const basePath = (FileSystem && FileSystem.documentDirectory) || '';
 
-const expoFsAsync = fileTransportFactory(
-  basePath,
-  () => async (filepath, contents, encoding) => {
-    const existingContents = await FileSystem.readAsStringAsync(filepath, {
+const getExistingFileContents = async (filepath: string) => {
+  try {
+    // NOTE: We need the `await` here for this catch block to catch
+    return await FileSystem.readAsStringAsync(filepath, {
       encoding: 'utf8',
     });
-    const newContents = `${existingContents}\n${contents}`;
+  } catch (error) {
+    // In the event of any error (expo's error codes are impossible to discern),
+    // check if the file exists, and if not, return an empty string, otherwise
+    // re throw whatever error we caught.
+    const stats = await FileSystem.getInfoAsync(filepath);
+    if (!stats.exists) {
+      return '';
+    }
+    throw error;
+  }
+};
+
+const expoFileSystemAsync = fileTransportFactory(
+  basePath,
+  () => async (filepath, contents, encoding) => {
+    const existingContents = await getExistingFileContents(filepath);
+    const newContents = `${existingContents}${contents}`;
     await FileSystem.writeAsStringAsync(filepath, newContents, {
       encoding: 'utf8',
     });
   }
 );
 
-export { expoFsAsync as rnFsFileAsync };
+export { expoFileSystemAsync };
